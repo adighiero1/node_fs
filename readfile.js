29fc749c0c2b481036e5d923a96c82cfb1eef72b
Alejandro Dighiero <adighiero1@gmail.com>
2023-05-02 10:15:07 -0400

    fixed

--- readfile.js
+++ readfile.js
@@ -1,9 +1,20 @@
 const { readFile } = require("fs");
+
 readFile("./HelloWorld.txt", (err, contents) => {
   if (err) {
     console.log(err);
   } else {
-    let content = data.toString();
-    console.log(contents.replace(/l/g, "1"));
+    let content = contents.toString();
+    console.log(content.replace(/l/g, "1"));
   }
 });
+
+// const { readFile } = require("fs");
+// readFile("./HelloWorld.txt", (err, contents) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     let content = data.toString();
+//     console.log(contents.replace(/l/g, "1"));
+//   }
+// });
